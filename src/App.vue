fedb0c2d9b771cbca9067b327858e2e8ab3307f6
Naif <nalh0006@student.monash.edu>
2025-08-02 21:08:57 +1000

    Add MyName.vue component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to My Vue.js App"/>
+    <MyName />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyName from './components/MyName.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyName
+  }
+}
+</script>
+
---
+++ src/components/MyName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-name">
+    <h2>Hello, my name is Naif Alharbi!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName'
+}
+</script>
+
+<style scoped>
+.my-name {
+  color: teal;
+  font-weight: bold;
+  margin-top: 20px;
+}
+</style>
